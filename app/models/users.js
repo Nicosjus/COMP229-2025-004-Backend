cb81313a39ee7e0bb5baccfafaab069580ad32f6
JViniX <47263194+JViniX@users.noreply.github.com>
2025-10-07 20:59:42 -0400

    Update users.js

---
+++ app/models/users.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String,
+        unique: true,
+        match: [/.+\@.+\..+/, "Please fill a valid e-mail address"]
+    },
+    usename: {
+        type: String,
+        unique: true,
+        required: 'Username is required',
+        trim: true
+    },
+    created: {
+        type: Date,
+        default: Date.now,
+        immutable: true
+    },
+    updated: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    collection: "users"
+});
+
+module.exports = mongoose.model("User", UserSchema);
