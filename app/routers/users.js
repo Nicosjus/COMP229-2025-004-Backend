8df7ecea0dcd6db9238a0f11bcd1cdfba7f6ba27
JViniX <47263194+JViniX@users.noreply.github.com>
2025-10-07 22:10:42 -0400

    Complete User CRUD

--- app/routers/users.js
+++ app/routers/users.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 var userController = require('../controllers/users');
 
-router.get('/getuser/:userId', userController.getUser);
+router.get('/', userController.getAll);
+router.post('/', userController.create);
+router.get('/:userId', userController.getUser);
+router.put('/:userId', userController.update);
+router.delete('/:userId', userController.remove);
 
 module.exports = router;
