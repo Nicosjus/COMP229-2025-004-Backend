883a546451f8cbf276d4294f379f3770773851cb
Nicosjus <nicosjus@gmail.com>
2025-12-01 06:00:15 -0500

    I have added the authentication

--- app/models/Users.js
+++ app/models/Users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
@@ -40,5 +41,20 @@ const UserSchema = new Schema(
         collection: "users"
     }
 );
+ // Hash password before saving
+UserSchema.pre('save', async function (next) {
+    try {
+        // Only hash if password is modified
+        if (!this.isModified('password')) {
+            return next();
+        }
+      // Hash the password
+        this.password = await bcrypt.hash(this.password, 17);
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
 
 module.exports = mongoose.model("User", UserSchema);
